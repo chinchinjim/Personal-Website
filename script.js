54495b0e780d1b5c75fefc7e2f9fc1043f0b0b40
Chin Chin Jim <chinchin.jim@mail.utoronto.ca>
2024-07-01 06:57:35 -0400

    fixed chatbot

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ var chatHistory = document.getElementById('chat-history');
     // Clear input field
     document.getElementById('zorgInput').value = '';
 
-    fetch('http://localhost:5000/chatbot_response?message=${userInput}')
+    fetch('/chatbot_response?message=${userInput}')
         .then(response => response.json())
         .then(data => {
             var zorgResponse = data.zorgResponse.replace(/\n/g, '<br>');
